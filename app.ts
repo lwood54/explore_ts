7c527f4268ca5552090292cadabb72c3118a5074
Logan Wood <lwood3499@gmail.com>
2020-10-10 15:46:57 -0500

    literal types & union types

--- app.ts
+++ app.ts
@@ -1,15 +1,24 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+  ) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number'
+    && typeof input2 === 'number'
+    || resultConversion === 'as-number') {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedAges = combine(30, 38);
+const combinedAges = combine(30, 38, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Logan', 'Tiffany');
+const combinedStringAges = combine('30', '38', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Logan', 'Tiffany', 'as-text');
 console.log(combinedNames);
