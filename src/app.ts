87a7ca2ec0cfbcc85eb826001cb333d0899c0025
Logan Wood <lwood3499@gmail.com>
2020-10-12 17:11:17 -0500

    intersection types

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,25 @@
-// type AddFn = (a: number, b: number) => number;
-// you can also create a function interface as functions are just objects
-interface AddFn { // you're not having to return anything, just defining types for each piece of the function
-  (a: number, b: number): number; // though the function type seems more straightforward to me
+// intersection types
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// this is an intersection type, but you could also do this by creating multiple interfaces, then extending
+type ElevatedEmployee = Admin & Employee; // object type that combines the two custom types
+
+const e1: ElevatedEmployee = {
+  name: 'Logan',
+  privileges: ['create-server'],
+  startDate: new Date()
 }
 
-let add: AddFn;
+// intersection types are also possible in other ways:
+type Pickable = string | number; // union type
+type Numeric = number | boolean;
+type Universal = Pickable & Numeric;
 
-add = (n1: number, n2: number) => {
-  return n1 + n2;
-}
-
-// interface is a TS key word
-interface Named {
-  readonly name?: string;
-  outputName?: string; // ? creates an optional property name
-}
-
-interface Greetable extends Named{ // you can extend an interface to another interface
-  greet(phrase: string): void;
-}
-
-// you can use an interface to create a 'contract' for what a class should contain or how it should be structured
-// you can implement multiple interfaces by using a ',' (unlike how you can only inherit one base class in another class)
-// this almost feels like a required todo list each time you create a class that will implement a given interface
-// class Person implements Greetable, Named { // you can either add extra interfaces like this OR
-class Person implements Greetable { // you can extend one interface from another, see above
-  name?: string;
-  age: number = 38;
-
-  constructor(n?: string) {
-    if (n) this.name = n;
-  }
-
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(phrase + ' ' + this.name);
-    } else {
-      console.log('Hello there!');
-    }
-  }
-
-}
-
-let user1: Greetable; // because I'm specifying this type interface, which does not have age, age cannot be called on
-// let user1; // I could either create another interface or do inferred type: any (which is never ideal)
-
-user1 = new Person(); //
-// user1 = new Person('Logan');
-
-user1.greet('Hi there, I am ');
-//console.log('I am ', user1.age); // cannot access age if the interface structure does not define it even if it is present on the class/object
-console.log('user1 object: ', user1);
