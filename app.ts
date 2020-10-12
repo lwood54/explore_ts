65f129828e4575881d5ae8528f3322efe49b1065
Logan Wood <lwood3499@gmail.com>
2020-10-12 07:19:22 -0500

    function types

--- app.ts
+++ app.ts
@@ -20,10 +20,23 @@
     return; // returns undefined
   }
 
+  function addAndHandle(n1:number, n2:number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+  }
+
   printResult(add(5, 12));
 
-  let combineValues;
+  // let combineValues: Function; // non-specific function type
+  let combineValues: (a: number, b: number) => number; // return type is a function that takes 2 params of type number and returns number
+
 
   combineValues = add;
+  // combineValues = printResult;
+  // combineValues = 5;
 
   console.log(combineValues(8, 8));
+
+  addAndHandle(56, 75, (result) => {
+    console.log(result);
+  })
