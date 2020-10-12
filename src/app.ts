c85394f6d861423188d018fa83c803a22cce76cf
Logan Wood <lwood3499@gmail.com>
2020-10-12 14:51:26 -0500

    implementing interfaces with classes

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
 // interface is a TS key word
-interface Person {
-  name: string;
-  age: number;
-
+interface Greetable {
+  readonly name: string;
+  // age: number;
   greet(phrase: string): void;
 }
 
-let user1: Person;
+// you can use an interface to create a 'contract' for what a class should contain or how it should be structured
+// you can implement multiple interfaces by using a ',' (unlike how you can only inherit one base class in another class)
+// this almost feels like a required todo list each time you create a class that will implement a given interface
+class Person implements Greetable {
+  name: string;
+  age: number = 38;
 
-user1 = {
-  name: 'Logan',
-  age: 38,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name);
   }
+
 }
 
+let user1: Greetable; // because I'm specifying this type interface, which does not have age, age cannot be called on
+// let user1; // I could either create another interface or do inferred type: any (which is never ideal)
+
+user1 = new Person('Logan');
+
 user1.greet('Hi there, I am ');
+//console.log('I am ', user1.age); // cannot access age if the interface structure does not define it even if it is present on the class/object
+console.log('user1 object: ', user1);
